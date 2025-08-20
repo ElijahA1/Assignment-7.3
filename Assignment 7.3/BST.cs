a14bdcdaf13be85c65651da792b8aff46fb54c11
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-20 11:10:30 -0700

    Add project files.

---
+++ Assignment 7.3/BST.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7._3
+{
+    internal class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+    internal class BST
+    {
+        public Node root; // starting point
+        public BST()
+        {
+            root = null;
+        }
+        public void InsertNewNode(Node tempRoot, int val)
+        {
+            Node node = new Node(val);
+            Node temp = null;
+            if (root != null)
+            {
+                while (tempRoot != null)
+                {
+                    temp = tempRoot;
+                    if (tempRoot.value == val)
+                    {
+                        Console.WriteLine("Value already exists in the tree.");
+                        return; // value already exists, do not insert again
+                    }
+                    else if (val < tempRoot.value)
+                    {
+                        tempRoot = tempRoot.left; // go left
+                    }
+                    else
+                    {
+                        tempRoot = tempRoot.right; // go right
+                    }
+
+                }
+                if (val < temp.value)
+                {
+                    temp.left = node; // insert as left child
+                }
+                else
+                {
+                    temp.right = node; // insert as right child
+                }
+            }
+            else
+            {
+                root = node; // if tree is empty, set root to new node
+            }
+        }
+        public void InOrderTraversal(Node tempRoot) //DFS
+        {
+            if (tempRoot != null)
+            {
+                InOrderTraversal(tempRoot.left);
+                Console.Write(tempRoot.value + " ");
+                InOrderTraversal(tempRoot.right);
+            }
+        }
+        public void PreOrderTraversal(Node tempRoot) //DFS
+        {
+            if (tempRoot != null)
+            {
+                Console.Write(tempRoot.value + " ");
+                InOrderTraversal(tempRoot.left);
+                InOrderTraversal(tempRoot.right);
+            }
+        }
+        public void PostOrderTraversal(Node tempRoot) //DFS
+        {
+            if (tempRoot != null)
+            {
+                InOrderTraversal(tempRoot.left);
+                InOrderTraversal(tempRoot.right);
+                Console.Write(tempRoot.value + " ");
+            }
+        }
+        public bool TrySearch(int val, out Node temp)
+        {
+            temp = root;
+            while (temp != null)
+            {
+                if (temp.value == val)
+                {
+                    return true;
+                }
+                else if (val < temp.value)
+                {
+                    temp = temp.left;
+                }
+                else
+                {
+                    temp = temp.right;
+                }
+            }
+            Console.Error.WriteLine("\nValue does not exist in current context");
+            return false;
+        }
+    }
+}
---
+++ Assignment 7.3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Assignment_7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BST bst = new();
+            bst.InsertNewNode(bst.root, 11);
+            bst.InsertNewNode(bst.root, 14);
+            bst.InsertNewNode(bst.root, 7);
+            bst.InsertNewNode(bst.root, 9);
+            bst.InsertNewNode(bst.root, 5);
+            bst.InsertNewNode(bst.root, 16);
+            bst.InsertNewNode(bst.root, 12);
+            Node realNode = new Node(7);
+            Node fakeNode = new Node(13);
+
+            if (bst.TrySearch(realNode.value, out Node temp)) // true
+                bst.InOrderTraversal(temp);
+
+            if (bst.TrySearch(fakeNode.value, out Node nullTemp)) // false
+                bst.InOrderTraversal(nullTemp); // Value Does Not exist in current context
+
+            Console.ReadKey();
+        }
+    }
+
+    /*   Tree for Reference
+     *          11(root)
+     *         /  \
+     *        7    14
+     *       /\    /\
+     *      5  9  12 16
+     */
+}
